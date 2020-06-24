d1c0162c266d880997de16f2f1a1958efdb539d9
Ilkka Mäkinen <ijmakine@gmail.com>
2020-06-24 10:18:11 +0300

    transactional due to postgre issue

--- src/main/java/projekti/repository/AccountRepository.java
+++ src/main/java/projekti/repository/AccountRepository.java
@@ -1,8 +1,10 @@
 package projekti.repository;
 
+import javax.transaction.Transactional;
 import projekti.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Transactional
 public interface AccountRepository extends JpaRepository<Account, Long> {
     Account findByUsername(String username);
     Account findBySlug(String slug);
